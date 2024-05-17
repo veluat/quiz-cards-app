2e1018f2c23736a7acfabec54a2a93a4d9b09871
Julia <veluat@gmail.com>
2024-05-17 21:55:15 +0300

    feat: add dark theme to Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,29 @@
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
+import '../src/styles/index.scss'
+
+import { themes } from '@storybook/theming'
 import type { Preview } from '@storybook/react'
-import '@/styles/index.scss'
 
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#000000',
+        },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
+        date: /Date$/,
       },
     },
   },
