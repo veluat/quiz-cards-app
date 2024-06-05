9d66db8921c9e8040ca8d3d10fa735bd5f6188c6
Julia <veluat@gmail.com>
2024-06-05 15:17:34 +0300

    style: checkbox & radio group optimized

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -13,36 +13,28 @@ export type CheckboxProps = {
   disabled?: boolean
   id?: string
   label?: string
-  onChange: (checked: boolean) => void
+  onChange?: (checked: boolean) => void
 }
 
 export const Checkbox: FC<CheckboxProps> = props => {
   const { checked, className, disabled, id, label, onChange } = props
 
-  const classes = {
-    checkboxWrapper: clsx(s.checkboxWrapper, disabled && s.disabled),
-    indicator: s.indicator,
-    label: clsx(s.label, disabled && s.disabled, className),
-    root: clsx(s.root),
-  }
-
   return (
-    <Typography as={'label'} className={classes.label}>
-      <div className={classes.checkboxWrapper}>
-        <RadixCheckbox.Root
-          checked={checked}
-          className={classes.root}
-          disabled={disabled}
-          id={id}
-          onCheckedChange={onChange}
-        >
-          {checked && (
-            <RadixCheckbox.Indicator className={classes.indicator} forceMount>
-              <Icon height={24} name={'checked'} width={24} />
-            </RadixCheckbox.Indicator>
-          )}
-        </RadixCheckbox.Root>
-      </div>
+    <Typography as={'label'} className={clsx(s.label, disabled && s.disabled, className)}>
+      <RadixCheckbox.Root
+        checked={checked}
+        className={s.checkbox}
+        disabled={disabled}
+        id={id}
+        onCheckedChange={onChange}
+      >
+        <div className={s.frame}></div>
+        {checked && (
+          <RadixCheckbox.Indicator className={s.indicator} forceMount>
+            <Icon height={24} name={'checked'} width={24} />
+          </RadixCheckbox.Indicator>
+        )}
+      </RadixCheckbox.Root>
       {label}
     </Typography>
   )
