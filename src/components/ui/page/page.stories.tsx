cae634306a8ecc47e88f1f23bbb4c41123504305
Julia <veluat@gmail.com>
2024-06-11 20:46:30 +0300

    feat: page stories add

--- src/components/ui/page/page.stories.tsx
+++ src/components/ui/page/page.stories.tsx
@@ -1,14 +1,24 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import { MemoryRouter } from 'react-router-dom'
+
+import { LoginForm } from '@/features'
+import { action } from '@storybook/addon-actions'
+
 import { Page } from './page'
 
 const meta = {
   component: Page,
   tags: ['autodocs'],
-  title: 'page/Page',
+  title: 'Components/Page',
 } satisfies Meta<typeof Page>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    children: <LoginForm onSubmit={data => action('SignIn data')(data)} />,
+  },
+  decorators: [story => <MemoryRouter>{story()}</MemoryRouter>],
+}
