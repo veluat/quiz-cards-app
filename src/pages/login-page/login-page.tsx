03f51810ff4e4a45f9997929a4e688557427aa4e
Julia <veluat@gmail.com>
2024-06-13 19:55:06 +0300

    fix: login page import fix

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,7 +1,6 @@
 import { Page } from '@/components/ui/page'
 import { LoginArgs } from '@/features/auth/api/types'
-
-import { LoginForm } from '../../features/auth/ui/login-form'
+import { LoginForm } from '@/features/auth/ui/login-form'
 
 export const LoginPage = () => {
   const loginHandler = (loginData: LoginArgs) => {
