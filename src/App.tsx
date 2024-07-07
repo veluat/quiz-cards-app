ff6940d4f314acd86bdef0d0cc5f66c6e68b77b6
Julia <veluat@gmail.com>
2024-07-07 13:12:01 +0300

    feat: add store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Provider } from 'react-redux'
+
 import { Router } from '@/common/routes/router'
+import { store } from '@/common/services/store'
 
 export function App() {
-  return <Router />
+  return (
+    <Provider store={store}>
+      <Router />
+    </Provider>
+  )
 }
