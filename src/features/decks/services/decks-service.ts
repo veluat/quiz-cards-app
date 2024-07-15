2fcf4984dccd22d91ef84525c20279f7ac7b3cbb
Julia <veluat@gmail.com>
2024-07-15 20:37:22 +0300

    feat: add decks api

---
+++ src/features/decks/services/decks-service.ts
@@ -0,0 +1,34 @@
+import { baseApi } from '@/common/services/base-api'
+import {
+  CreateDeckArgs,
+  GetDeckByIdArgs,
+  GetDecksArgs,
+  GetDecksResponse,
+} from '@/features/decks/services/decks-types'
+
+export const decksService = baseApi.injectEndpoints({
+  endpoints: builder => {
+    return {
+      createDeck: builder.mutation<void, CreateDeckArgs>({
+        invalidatesTags: ['Decks'],
+        query: args => ({
+          body: args,
+          method: 'POST',
+          url: `v1/decks`,
+        }),
+      }),
+      getDeckById: builder.query<GetDecksResponse, GetDeckByIdArgs>({
+        query: ({ id }) => `v1/decks/${id}`,
+      }),
+      getDecks: builder.query<GetDecksResponse, GetDecksArgs | void>({
+        providesTags: ['Decks'],
+        query: args => ({
+          params: args ?? {},
+          url: `v2/decks`,
+        }),
+      }),
+    }
+  },
+})
+
+export const { useCreateDeckMutation, useGetDeckByIdQuery, useGetDecksQuery } = decksService
