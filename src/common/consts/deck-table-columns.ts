3d58608d2c27a49b4e2ef61f96f5f5f4d2e4de13
Julia <veluat@gmail.com>
2024-07-18 20:15:21 +0300

    feat: add deck page

---
+++ src/common/consts/deck-table-columns.ts
@@ -0,0 +1,23 @@
+export const deckTableColumns = [
+  {
+    key: 'question',
+    title: 'Question',
+  },
+  {
+    key: 'answer',
+    title: 'Answer',
+  },
+  {
+    key: 'updated',
+    title: 'Last Updated',
+  },
+  {
+    key: 'grade',
+    title: 'Grade',
+  },
+  {
+    key: 'controls',
+    sortable: false,
+    title: '',
+  },
+]
