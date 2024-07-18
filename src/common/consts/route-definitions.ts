fa693e0cb834b8c3b9c9c97d0595d93aaa698649
Julia <veluat@gmail.com>
2024-07-18 20:18:28 +0300

    feat: add const route definitions

---
+++ src/common/consts/route-definitions.ts
@@ -0,0 +1,12 @@
+export const RouteDefinitions = {
+  base: '/',
+  checkEmail: '/check-email',
+  createNewPassword: '/create-new-password',
+  decks: '/decks',
+  learn: '/learn',
+  profile: '/profile',
+  recoverPassword: '/recover-password',
+  rest: '/*',
+  signIn: '/login',
+  signUp: '/sign-up',
+} as const
