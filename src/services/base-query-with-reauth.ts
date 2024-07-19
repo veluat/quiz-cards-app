ee34b2a25dc0c416b5b4bfec6352b3228be20fb6
Julia <veluat@gmail.com>
2024-07-19 08:47:17 +0300

    ref: refactored base api and base api with reauth

---
+++ src/services/base-query-with-reauth.ts
@@ -0,0 +1,48 @@
+import {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+  fetchBaseQuery,
+} from '@reduxjs/toolkit/query/react'
+import { Mutex } from 'async-mutex'
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'https://api.flashcards.andrii.es',
+  credentials: 'include',
+})
+
+const mutex = new Mutex()
+
+export const baseQueryWithReauth: BaseQueryFn<
+  FetchArgs | string,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  await mutex.waitForUnlock()
+  let result = await baseQuery(args, api, extraOptions)
+
+  if (result.error && result.error.status === 401) {
+    if (!mutex.isLocked()) {
+      const release = await mutex.acquire()
+
+      try {
+        const refreshResult = await baseQuery(
+          { method: 'POST', url: '/v2/auth/refresh-token' },
+          api,
+          extraOptions
+        )
+
+        if (refreshResult?.meta?.response?.status === 204) {
+          result = await baseQuery(args, api, extraOptions)
+        }
+      } finally {
+        release()
+      }
+    } else {
+      await mutex.waitForUnlock()
+      result = await baseQuery(args, api, extraOptions)
+    }
+  }
+
+  return result
+}
